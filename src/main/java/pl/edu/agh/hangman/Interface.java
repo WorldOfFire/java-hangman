34d168f1c36400970b79328fb95ae8c225235f27
zwawro <zuzanna.wawro7@gmail.com>
2025-02-09 15:30:57 +0100

    Interface  implemented while looop added, hangman class added

--- src/main/java/pl/edu/agh/hangman/Interface.java
+++ src/main/java/pl/edu/agh/hangman/Interface.java
@@ -9,19 +9,28 @@ public class Interface {
         String randomWord = wordProvider.getRandom();
         System.out.println(randomWord);
 
-        LogicGame logicGame = new LogicGame(randomWord);
+        LogicGame logicGame = new LogicGame(randomWord.toLowerCase());
         char[] currentCharArray  = new char[logicGame.getWordCharArray().length];
         for (int i = 0; i < logicGame.getWordCharArray().length; i++){
             currentCharArray[i] = '-';
         }
         System.out.println(currentCharArray);
 
-        char yourGuess = wordScanner();
-        System.out.println(yourGuess);
+        int chancesCounter = 0;
+        while (chancesCounter < 7) {
+            char yourGuess = wordScanner();
+            System.out.println(yourGuess);
 
-        System.out.println(logicGame.isGuessCorrect(yourGuess, currentCharArray));
-        System.out.println(currentCharArray);
+            System.out.println(logicGame.isGuessCorrect(yourGuess, currentCharArray));
+            System.out.println(currentCharArray);
+            if (!logicGame.isGuessCorrect(yourGuess, currentCharArray)) {
+                HangmanPics hangmanPics = new HangmanPics();
+                hangmanPics.getHangmanpics();
+                System.out.println();
+                chancesCounter++;
 
+            }
+        }
     }
 
     public char wordScanner() {
